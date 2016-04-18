286142cbaf1daa79c0f61fa705de45efca41f83b
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-04-18 14:55:37 -0300

    A+

--- EP3.py
+++ EP3.py
@@ -7,39 +7,42 @@ class tabuleiro:
         #DANDO UM NOME A JANELA
         self.window.title("Jogo da velha")
         self.window.geometry("300x300")
-        self.window.rowconfigure(0, minisize=100,weight=1)
-        self.window.rowconfigure(1,minisize=100,weight=1)
-        self.window.rowconfigure(2, minisize=100,weight=1)
-        self.window.columnconfigure(0, minisize=100,weight=1)
-        self.window.columnconfigure(1, minisize=100,weight=1)
-        self.window.columnconfigure(2, minisize=100,weight=1)
-        botao_00 = tk.Button (window) #00 é a localização do botao esse no caso é o superior esquerdo
+        self.window.rowconfigure(0, minsize=100,weight=1)
+        self.window.rowconfigure(1,minsize=100,weight=1)
+        self.window.rowconfigure(2, minsize=100,weight=1)
+        self.window.columnconfigure(0, minsize=100,weight=1)
+        self.window.columnconfigure(1, minsize=100,weight=1)
+        self.window.columnconfigure(2, minsize=100,weight=1)
+        botao_00 = tk.Button (self.window) #00 é a localização do botao esse no caso é o superior esquerdo
         botao_00.grid(row=0,column=0,sticky="nswe")
 
-        botao_01 = tk.Button (window)
+        botao_01 = tk.Button (self.window)
         botao_01.grid(row=0,column=1,sticky="nswe")
 
-        botao_02 =tk.Button (window)
+        botao_02 =tk.Button (self.window)
         botao_02.grid (row=0,column=2,sticky="nswe")
 
-        botao_10=tk.Button (window)
+        botao_10=tk.Button (self.window)
         botao_10.grid(row=1,column=0,sticky="nswe")
 
-        botao_20=tk.Button (window)
+        botao_20=tk.Button (self.window)
         botao_20.grid(row=2,column=0,sticky="nswe")
 
-        botao_11=tk.Button (window)
+        botao_11=tk.Button (self.window)
         botao_11.grid(row=1,column=1,sticky="nswe")
 
-        botao_21=tk.Button (window)
+        botao_21=tk.Button (self.window)
         botao_21.grid(row=2,column=1,sticky="nswe")
 
-        botao_12=tk.Button (window)
+        botao_12=tk.Button (self.window)
         botao_12.grid(row=1,column=2,sticky="nswe")
 
-        botao_22 = tk.Button (window)
+        botao_22 = tk.Button (self.window)
         botao_22.grid(row=2,column=2,sticky="nswe")
 
 
 
-        window.mainloop()
+    def iniciar(self):
+        self.window.mainloop()
+app= tabuleiro()
+app.iniciar()
