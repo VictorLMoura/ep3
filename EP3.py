4a1d162b8796191d2880e7c5ff2671d2f99432e4
LeonardoGrotti <leo.grotti1@gmail.com>
2016-04-25 15:48:45 -0300

    A+

--- EP3.py
+++ EP3.py
@@ -5,6 +5,7 @@ class tabuleiro:
         #import Jogo #, precisamos importar a classe Jogo para usar o atributo jogador
 #CRIANDO OS BOTOES    
         self.jogador=1
+        self.venceu = False
         self.window = tk.Tk()
         #DANDO UM NOME A JANELA
         self.window.title("Jogo da Velha!")
@@ -37,6 +38,7 @@ class tabuleiro:
         self.botao_10.grid(row=1,column=0,sticky="nsew")
         self.botao_10.configure(command= self.botao_10clicado)
         
+        
         self.botao_20=tk.Button (self.window)
         self.botao_20.grid(row=2,column=0,sticky="nsew")
         self.botao_20.configure(command= self.botao_20clicado)
@@ -59,85 +61,144 @@ class tabuleiro:
         
         self.label_vez = tk.Label(self.window)
         self.label_vez.configure(font="Courier 14 bold")
-        self.label_vez.grid(row = 3, column = 0, columnspan=2)
+        self.label_vez.grid(row = 3, column = 0, columnspan=3)
         self.label_vez.configure(text='Vez do: Jogador 1')
+        
+        self.B_00_clicado = False
+        self.B_10_clicado = False
+        self.B_20_clicado = False
+        self.B_11_clicado = False
+        self.B_12_clicado = False
+        self.B_01_clicado = False
+        self.B_02_clicado = False
+        self.B_22_clicado = False
+        self.B_21_clicado = False
+        
+        
         #Precisamos descobrir como importar a classe Jogo para usar a muda_jogador
 
   #AÇÕES DOS BOTÕES , SÓ UM EXEMPLO
     def clique (self, i, j):
-        print ("botao {0} x {1} clicado".format(i,j))
+        print ("botao {0} x {1} clicado".format(i,j))        
+       
+        
     def botao_00clicado(self):
-        self.clique(0,0)
-        if(self.jogador==1):
-            self.botao_00.configure(text = "X")
-        else:
-            self.botao_00.configure(text = "O")
-        self.muda_jogador()
-        verifica_vencedor()
+        if (self.B_00_clicado == False):
+            if (self.venceu==False):
+                self.B_00_clicado = True
+                self.clique(0,0)
+                if(self.jogador==1):
+                    self.botao_00.configure(text = "X")
+                else:
+                    self.botao_00.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
        # recebe_jogada(0,0)
     def botao_01clicado(self):
-        self.clique(0,1)
-        if(self.jogador==1):
-            self.botao_01.configure(text = "X")
-        else:
-            self.botao_01.configure(text = "O")   
-        self.muda_jogador()
+        if (self.B_01_clicado == False):
+            if (self.venceu==False):
+                self.B_01_clicado = True
+                self.clique(0,1)
+                if(self.jogador==1):
+                    self.botao_01.configure(text = "X")
+                else:
+                    self.botao_01.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
+        
       #  recebe_jogada(0,1)
     def botao_10clicado(self):
-        self.clique(1,0)
-        if(self.jogador==1):
-            self.botao_10.configure(text = "X")
-        else:
-            self.botao_10.configure(text = "O")
-        self.muda_jogador()
-     #   recebe_jogada(1,0)
+        if (self.B_10_clicado == False):
+            if (self.venceu==False):
+                self.B_10_clicado = True
+                self.clique(1,0)
+                if(self.jogador==1):
+                    self.botao_10.configure(text = "X")
+                else:
+                    self.botao_10.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
+     #      recebe_jogada(1,0)
     def botao_20clicado(self):
-        self.clique(2,0)
-        if(self.jogador==1):
-            self.botao_20.configure(text = "X")
-        else:
-            self.botao_20.configure(text = "O")
-        self.muda_jogador()
+        if (self.B_20_clicado == False):
+            if (self.venceu==False):
+                self.B_20_clicado = True
+                self.clique(2,0)
+                if(self.jogador==1):
+                    self.botao_20.configure(text = "X")
+                else:
+                    self.botao_20.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+           
       #  recebe_jogada(2,0)
     def botao_11clicado(self):
-        self.clique(1,1)
-        if(self.jogador==1): 
-            self.botao_11.configure(text = "X")
-        else:
-            self.botao_11.configure(text = "O")
-        self.muda_jogador()
+        if (self.B_11_clicado == False):
+            if (self.venceu==False):
+                self.B_11_clicado = True
+                self.clique(1,1)
+                if(self.jogador==1):
+                    self.botao_11.configure(text = "X")
+                else:
+                    self.botao_11.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
       #  recebe_jogada(1,1)
     def botao_12clicado(self):
-        self.clique(1,2)
-        if(self.jogador==1):
-            self.botao_12.configure(text = "X")
-        else:
-            self.botao_12.configure(text = "O")
-        self.muda_jogador()
+        if (self.B_12_clicado == False):
+            if (self.venceu==False):
+                self.B_12_clicado = True
+                self.clique(1,2)
+                if(self.jogador==1):
+                    self.botao_12.configure(text = "X")
+                else:
+                    self.botao_12.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
       #  recebe_jogada(1,2)
     def botao_21clicado(self):
-        self.clique(2,1)
-        if(self.jogador==1):
-            self.botao_21.configure(text = "X")
-        else:
-            self.botao_21.configure(text = "O")
-        self.muda_jogador()
+      if (self.B_21_clicado == False):
+            if (self.venceu==False):
+                self.B_21_clicado = True
+                self.clique(2,1)
+                if(self.jogador==1):
+                    self.botao_21.configure(text = "X")
+                else:
+                    self.botao_21.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
       #  recebe_jogada(2,1)
     def botao_22clicado(self):
-        self.clique(2,2)
-        if(self.jogador==1):
-            self.botao_22.configure(text = "X")
-        else:
-            self.botao_22.configure(text = "O")
-        self.muda_jogador()
+        if (self.B_22_clicado == False):
+            if (self.venceu==False):
+                self.B_22_clicado = True
+                self.clique(2,2)
+                if(self.jogador==1):
+                    self.botao_22.configure(text = "X")
+                else:
+                    self.botao_22.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
      #   recebe_jogada(2,2)
     def botao_02clicado(self):
-        self.clique(0,2)
-        if(self.jogador==1):
-            self.botao_02.configure(text = "X")
-        else:
-            self.botao_02.configure(text = "O")
-        self.muda_jogador()
+        if (self.B_02_clicado == False):
+            if (self.venceu==False):
+                self.B_02_clicado = True
+                self.clique(0,2)
+                if(self.jogador==1):
+                    self.botao_02.configure(text = "X")
+                else:
+                    self.botao_02.configure(text = "O")
+                self.muda_jogador()
+                self.verifica_vencedor()
+            
       #  recebe_jogada(0,2)
 
     def iniciar(self):
@@ -146,15 +207,35 @@ class tabuleiro:
     def muda_jogador(self):
         if (self.jogador==1):
             self.label_vez.configure(text='Vez do: Jogador 2')
-            self.jogador = 1 + 1
-            
-
-        
+            self.jogador = 2
+                    
         elif(self.jogador==2):
             self.label_vez.configure(text='Vez do: Jogador 1')
-            self.jogador= 2 - 1
+            self.jogador= 1
+            
+    def X_vencedor (self):
+        self.label_vez.configure(text='Vencedor: Jogador 1')
+        self.venceu=True
+        
+    def Y_vencedor (self):
+         self.label_vez.configure(text='Vencedor: Jogador 2')
+         self.venceu=True
+         
+    def verifica_vencedor (self):
+      
+        if (self.botao_00["text"] =="X" and  self.botao_01["text"] =="X" and  self.botao_02["text"] =="X") or (self.botao_00["text"] =="X" and  self.botao_10["text"] =="X" and  self.botao_20["text"] =="X") or (self.botao_00["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_10["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_12["text"] =="X") or (self.botao_20["text"] =="X" and  self.botao_21["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_01["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_21["text"] =="X") or (self.botao_02["text"] =="X" and  self.botao_12["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_02["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_20["text"] =="X")  :
+            self.X_vencedor () 
+            
+                               
+        elif (self.botao_00["text"] =="O" and  self.botao_01["text"] =="O" and  self.botao_02["text"] =="O") or (self.botao_00["text"] =="O" and  self.botao_10["text"] =="O" and  self.botao_20["text"] =="O") or (self.botao_00["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_10["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_12["text"] =="O") or (self.botao_20["text"] =="O" and  self.botao_21["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_01["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_21["text"] =="O") or (self.botao_02["text"] =="O" and  self.botao_12["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_02["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_20["text"] =="O")  :
+            self.Y_vencedor () 
+            
+            
+        elif (self.B_00_clicado == True) and (self.B_01_clicado == True) and (self.B_02_clicado == True) and (self.B_10_clicado == True) and (self.B_11_clicado == True) and (self.B_12_clicado == True) and (self.B_20_clicado == True) and (self.B_21_clicado == True) and (self.B_22_clicado == True):
+            self.label_vez.configure(text='Deu Velha!')
+            self.venceu=True 
            
-        print (self.jogador)
+        
         
 
 class Jogo:
@@ -162,65 +243,8 @@ class Jogo:
         import tabuleiro
         #self.jogador=1
 
-    def recebe_jogada(self,i,j):
-         self.muda_jogador()     
+         
         
-#    def muda_jogador(self):
-#        if (self.jogador==1):
-#            self.label_vez.configure(text='Jogador 2')
-#            self.jogador=2
-#            
-#        if (self.jogador==2):
-#            self.label_vez.configure(text='Jogador 1')
-#            self.jogador=1
-#    
-    def X_vencedor (self):
-        self.botao_vencedor =  tk.Button(self.window)
-        self.botao_vencedor.configure(text= "O jogador 1 venceu")
-        
-    def Y_vencedor (self):
-        self.botao_vencedor =  tk.Button(self.window)
-        self.botao_vencedor.configure(text= "O jogador 2 venceu")
-    
-    def verifica_vencedor (self):
-        #AQUI O QUE EU PENSEI FOI QUE EXISTEM OITO COMBINAÇOES QUE FAZEM COM QUE ALGUEM GANHE AI EU COLOQUEI AS OITO PRO JOGADOR X PARA VER SE RODAVA
-    # NAO SEI PQ A FUNCAO NAO TA RECONHECENDO OS NOMES DO BOTAO ENTAO POR ISSO NAO TA RODANDO MAS FORA ISSO ACHO QUE TA CERTO
-        if self.botao_00["text"] =="X" and  self.botao_01["text"] =="X" and  self.botao_02["text"] =="X" :
-            self.X_vencedor ()
-        if self.botao_00["text"] =="X" and  self.botao_10["text"] =="X" and  self.botao_20["text"] =="X" :
-            self.X_vencedor ()
-        if self.botao_00["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_22["text"] =="X" :
-            self.X_vencedor ()
-        if self.botao_10["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_12["text"] =="X" :
-            self.X_vencedor ()
-        if self.botao_20["text"] =="X" and  self.botao_21["text"] =="X" and  self.botao_22["text"] =="X" :
-            self.X_vencedor ()
-        if self.botao_01["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_21["text"] =="X" :
-           self.X_vencedor ()
-        if self.botao_02["text"] =="X" and  self.botao_12["text"] =="X" and  self.botao_22["text"] =="X" :
-           self.X_vencedor ()
-        if self.botao_02["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_20["text"] =="X" :
-            self.X_vencedor () 
-            
-        #AQUI EU FIZ EXATAMENTE A MESMA COISA SO QUE PRO JOGADOR O            
-            
-        if self.botao_00["text"] =="O" and  self.botao_01["text"] =="O" and  self.botao_02["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_00["text"] =="O" and  self.botao_10["text"] =="O" and  self.botao_20["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_00["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_22["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_10["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_12["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_20["text"] =="O" and  self.botao_21["text"] =="O" and  self.botao_22["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_01["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_21["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_02["text"] =="O" and  self.botao_12["text"] =="O" and  self.botao_22["text"] =="O" :
-            self.Y_vencedor ()
-        if self.botao_02["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_20["text"] =="O" :
-            self.Y_vencedor () 
+
 app= tabuleiro()
 app.iniciar()
-
-        
