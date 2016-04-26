2e0308d6863b8e73dc1cf795d07f4e9cc2718d35
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-04-26 17:41:53 -0300

    A+

--- Jogo.py
+++ Jogo.py
@@ -1,7 +1,7 @@
 class Jogo:
     def __init__(self):
         self.jogador=1
-        self.venceu = False
+        self.venceu = 0
         self.vencedor= 0
         self.B_00_clicado = 0
         self.B_10_clicado = 0
@@ -22,22 +22,22 @@ class Jogo:
           self.jogador= 1
                 
     def X_vencedor (self):
-      self.label_vez.configure(text='Vencedor: Jogador 1')
-      self.venceu=True
+      self.venceu=1
       self.vencedor=1
+      self.quem_venceu()
+      
     def Y_vencedor (self):
-      self.label_vez.configure(text='Vencedor: Jogador 2')
-      self.venceu=True
+      self.venceu=2
       self.vencedor=2
-    
+     
     def Velha (self):
-      self.venceu=True
+      self.venceu=3
       self.vencedor=3
          
     def verifica_vencedor (self):
       
       if (self.B_00_clicado==1 and  self.B_01_clicado==1 and  self.B_02_clicado==1) or (self.B_00_clicado==1 and  self.B_10_clicado==1 and  self.B_20_clicado==1) or (self.B_00_clicado==1 and  self.B_11_clicado==1 and  self.B_22_clicado==1) or (self.B_10_clicado==1 and  self.B_11_clicado==1 and  self.B_12_clicado==1) or (self.B_20_clicado==1 and  self.B_21_clicado==1 and  self.B_22_clicado==1) or (self.B_01_clicado==1 and  self.B_11_clicado==1 and  self.B_21_clicado==1) or (self.B_02_clicado==1 and  self.B_12_clicado==1 and  self.B_22_clicado==1) or (self.B_02_clicado==1 and  self.B_11_clicado==1 and  self.B_20_clicado==1)  :
-          self.X_vencedor ()   
+          self.X_vencedor () 
          
             
       if (self.B_00_clicado==2 and  self.B_01_clicado==2 and  self.B_02_clicado==2) or (self.B_00_clicado==2 and  self.B_10_clicado==2 and  self.B_20_clicado==2) or (self.B_00_clicado==2 and  self.B_11_clicado==2 and  self.B_22_clicado==2) or (self.B_10_clicado==2 and  self.B_11_clicado==2 and  self.B_12_clicado==2) or (self.B_20_clicado==2 and  self.B_21_clicado==2 and  self.B_22_clicado==2) or (self.B_01_clicado==2 and  self.B_11_clicado==2 and  self.B_21_clicado==2) or (self.B_02_clicado==2 and  self.B_12_clicado==2 and  self.B_22_clicado==2) or (self.B_02_clicado==2 and  self.B_11_clicado==2 and  self.B_20_clicado==2)  :
