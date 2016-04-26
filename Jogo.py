87ad4a7d4825d2a99fa93532990f9cbf0f766507
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-04-26 17:23:12 -0300

    A+

--- Jogo.py
+++ Jogo.py
@@ -3,7 +3,17 @@ class Jogo:
         self.jogador=1
         self.venceu = False
         self.vencedor= 0
-      
+        self.B_00_clicado = 0
+        self.B_10_clicado = 0
+        self.B_20_clicado = 0
+        self.B_11_clicado = 0
+        self.B_12_clicado = 0
+        self.B_01_clicado = 0
+        self.B_02_clicado = 0
+        self.B_22_clicado = 0
+        self.B_21_clicado = 0
+        
+       
     def muda_jogador(self):
       if (self.jogador==1):
           self.jogador = 2
@@ -26,16 +36,15 @@ class Jogo:
          
     def verifica_vencedor (self):
       
-      if (self.botao_00["text"] =="X" and  self.botao_01["text"] =="X" and  self.botao_02["text"] =="X") or (self.botao_00["text"] =="X" and  self.botao_10["text"] =="X" and  self.botao_20["text"] =="X") or (self.botao_00["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_10["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_12["text"] =="X") or (self.botao_20["text"] =="X" and  self.botao_21["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_01["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_21["text"] =="X") or (self.botao_02["text"] =="X" and  self.botao_12["text"] =="X" and  self.botao_22["text"] =="X") or (self.botao_02["text"] =="X" and  self.botao_11["text"] =="X" and  self.botao_20["text"] =="X")  :
+      if (self.B_00_clicado==1 and  self.B_01_clicado==1 and  self.B_02_clicado==1) or (self.B_00_clicado==1 and  self.B_10_clicado==1 and  self.B_20_clicado==1) or (self.B_00_clicado==1 and  self.B_11_clicado==1 and  self.B_22_clicado==1) or (self.B_10_clicado==1 and  self.B_11_clicado==1 and  self.B_12_clicado==1) or (self.B_20_clicado==1 and  self.B_21_clicado==1 and  self.B_22_clicado==1) or (self.B_01_clicado==1 and  self.B_11_clicado==1 and  self.B_21_clicado==1) or (self.B_02_clicado==1 and  self.B_12_clicado==1 and  self.B_22_clicado==1) or (self.B_02_clicado==1 and  self.B_11_clicado==1 and  self.B_20_clicado==1)  :
           self.X_vencedor ()   
          
             
-                                
-      elif (self.botao_00["text"] =="O" and  self.botao_01["text"] =="O" and  self.botao_02["text"] =="O") or (self.botao_00["text"] =="O" and  self.botao_10["text"] =="O" and  self.botao_20["text"] =="O") or (self.botao_00["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_10["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_12["text"] =="O") or (self.botao_20["text"] =="O" and  self.botao_21["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_01["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_21["text"] =="O") or (self.botao_02["text"] =="O" and  self.botao_12["text"] =="O" and  self.botao_22["text"] =="O") or (self.botao_02["text"] =="O" and  self.botao_11["text"] =="O" and  self.botao_20["text"] =="O")  :
+      if (self.B_00_clicado==2 and  self.B_01_clicado==2 and  self.B_02_clicado==2) or (self.B_00_clicado==2 and  self.B_10_clicado==2 and  self.B_20_clicado==2) or (self.B_00_clicado==2 and  self.B_11_clicado==2 and  self.B_22_clicado==2) or (self.B_10_clicado==2 and  self.B_11_clicado==2 and  self.B_12_clicado==2) or (self.B_20_clicado==2 and  self.B_21_clicado==2 and  self.B_22_clicado==2) or (self.B_01_clicado==2 and  self.B_11_clicado==2 and  self.B_21_clicado==2) or (self.B_02_clicado==2 and  self.B_12_clicado==2 and  self.B_22_clicado==2) or (self.B_02_clicado==2 and  self.B_11_clicado==2 and  self.B_20_clicado==2)  :
          self.Y_vencedor () 
             
             
-      elif (self.B_00_clicado == True) and (self.B_01_clicado == True) and (self.B_02_clicado == True) and (self.B_10_clicado == True) and (self.B_11_clicado == True) and (self.B_12_clicado == True) and (self.B_20_clicado == True) and (self.B_21_clicado == True) and (self.B_22_clicado == True):
+      elif (self.B_00_clicado != 0) and (self.B_01_clicado != 0) and (self.B_02_clicado != 0) and (self.B_10_clicado != 0) and (self.B_11_clicado != 0) and (self.B_12_clicado != 0) and (self.B_20_clicado != 0) and (self.B_21_clicado != 0) and (self.B_22_clicado != 0):
          self.label_vez.configure(text='Deu Velha!')
-         self.venceu=True 
+         self.Velha()
            
